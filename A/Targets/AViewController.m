62ed272482671218e46620c5168802f6ffff6ec3
qihang <854883819@qq.com>
2019-10-16 17:51:37 +0800

    2

---
+++ A/Targets/AViewController.m
@@ -0,0 +1,35 @@
+//
+//  AViewController.m
+//  A
+//
+//  Created by 汪启航 on 2019/10/16.
+//  Copyright © 2019年 q.h. All rights reserved.
+//
+
+#import "AViewController.h"
+#import <B_Category/CTMediator+B.h>
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    UIViewController *viewController = [[CTMediator sharedInstance] B_viewControllerWithContentText:@"hello, world!"];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ A/Targets/Target_A.h
@@ -0,0 +1,18 @@
+//
+//  Target_A.h
+//  A
+//
+//  Created by 汪启航 on 2019/10/16.
+//  Copyright © 2019年 q.h. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Target_A : NSObject
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params;
+@end
+
+NS_ASSUME_NONNULL_END
